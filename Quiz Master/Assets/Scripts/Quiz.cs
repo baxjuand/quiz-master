dbf71a05bc7f3ae0e88216538d60f58ca07299ba
baxjuand <baxjuand@gmail.com>
2024-09-11 20:15:17 -0500

    Added quiz script and timer script. Included timer assets.

--- Quiz Master/Assets/Scripts/Quiz.cs
+++ Quiz Master/Assets/Scripts/Quiz.cs
@@ -1,12 +1,59 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSO question;
     [SerializeField] GameObject[] answerButtons;
+    int correctAnswerIndex;
+    Image buttonImage;
+    TextMeshProUGUI buttonTextColor;
+    [SerializeField] Sprite defaultAnswerSprite;
+    [SerializeField] Sprite correctAnswerSprite;
+    [SerializeField] Color correctAnswerTextColor;
+    [SerializeField] Color defaultAnswerTextColor;
     void Start()
+    {
+        //DisplayQuestion();
+        GetNextQuestion();
+    }
+
+    public void OnAnswerSelected(int index)
+    {
+        
+
+        if (index == question.GetCorrectAnswerIndex())
+        {
+            questionText.text = "Correct!";
+            buttonTextColor = answerButtons[index].GetComponentInChildren<TextMeshProUGUI>();
+            buttonTextColor.color = correctAnswerTextColor;
+            buttonImage = answerButtons[index].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        }
+        else
+        {
+            correctAnswerIndex = question.GetCorrectAnswerIndex();
+            string correctAnswer = question.GetAnswer(correctAnswerIndex);
+            questionText.text = "Wrong. The correct answer is:\n" + correctAnswer;
+            buttonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+            buttonTextColor = answerButtons[correctAnswerIndex].GetComponentInChildren<TextMeshProUGUI>();
+            buttonTextColor.color = correctAnswerTextColor;
+        }
+
+        SetButtonState(false);
+    }
+
+    void GetNextQuestion()
+    {
+        SetButtonState(true);
+        SetDefaultButtonApperance();
+        DisplayQuestion();
+    }
+
+    void DisplayQuestion()
     {
         questionText.text = question.GetQuestion();
 
@@ -16,11 +63,27 @@ public class Quiz : MonoBehaviour
             TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
             buttonText.text = question.GetAnswer(i);
         }
-
-        
-        
     }
 
+    void SetButtonState(bool state)
+    {
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            Button button = answerButtons[i].GetComponent<Button>();
+            button.interactable = state;
 
+        }
+    }
 
+    void SetDefaultButtonApperance()
+    {
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            buttonImage = answerButtons[i].GetComponent<Image>();
+            buttonImage.sprite = defaultAnswerSprite;
+            buttonTextColor = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttonTextColor.color = defaultAnswerTextColor;
+        }
+
+    }
 }
---
+++ Quiz Master/Assets/Scripts/Timer.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] float timeToCompleteQuestion = 20f;
+    [SerializeField] float timeToShowCorrectAnswer = 7f;
+    
+    public bool loadNextQuestion;
+    public bool isAnsweringQuestion;
+    public float fillFraction;
+    float timerValue;
+
+    void Update()
+    {
+        UpdateTimer();
+    }
+
+    public void CancelTimer()
+    {
+        timerValue = 0;
+    }
+
+    void UpdateTimer()
+    {
+        timerValue -= Time.deltaTime;
+
+        if (isAnsweringQuestion)
+        {
+            if (timerValue > 0)
+            {
+                fillFraction = timerValue / timeToCompleteQuestion;
+            }
+            else
+            {
+                isAnsweringQuestion = false;
+                timerValue = timeToShowCorrectAnswer;
+            }
+        }
+        else
+        {
+            if (timerValue > 0)
+            {
+                fillFraction = timerValue / timeToShowCorrectAnswer;
+            }
+            else
+            {
+                isAnsweringQuestion = true;
+                timerValue = timeToCompleteQuestion;
+                loadNextQuestion = true;
+            }
+        }
+        
+
+        Debug.Log(isAnsweringQuestion + ": " + timerValue + " = " + fillFraction);
+    }
+}
